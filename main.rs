ea198eb3fc7cf4e11abd23b32d3c8b04a2dac348
fgken <tmp@tmp.com>
2015-03-13 01:03:40 +0900

    Use Standard Libarary. Without stdlib(crate core), we can not use even 'for'(iterator)

--- main.rs
+++ main.rs
@@ -1,87 +1,22 @@
-#![feature(no_std)]
-#![no_std]
-#![allow(improper_ctypes)]
+#![feature(core)]
 
-#![feature(lang_items)]
-
-#[lang="phantom_fn"]
-trait PhantomFn<A: ?Sized, R: ?Sized = ()> {}
-
-#[lang="sized"]
-trait Sized: PhantomFn<Self> {}
-
-#[lang="copy"]
-trait Copy: PhantomFn<Self> {}
-
-impl Copy for Color {}
-
-#[allow(dead_code)]
-enum Color {
-    Black      = 0,
-    Blue       = 1,
-    Green      = 2,
-    Cyan       = 3,
-    Red        = 4,
-    Pink       = 5,
-    Brown      = 6,
-    LightGray  = 7,
-    DarkGray   = 8,
-    LightBlue  = 9,
-    LightGreen = 10,
-    LightCyan  = 11,
-    LightRed   = 12,
-    LightPink  = 13,
-    Yellow     = 14,
-    White      = 15,
-}
-
-enum Option<T> {
-    None,
-    Some(T)
-}
-
-struct IntRange {
-    cur: isize,
-    max: isize
-}
-
-impl IntRange {
-    fn next(&mut self) -> Option<isize> {
-        if self.cur < self.max {
-            self.cur += 1;
-            Option::Some(self.cur - 1)
-        } else {
-            Option::None
-        }
-    }
-}
-
-fn range(lo: isize, hi: isize) -> IntRange {
-    IntRange { cur: lo, max: hi }
-}
-
-fn clear_screen(background: Color) {
-    let mut r = range(0, 80 * 25);
-    loop {
-        match r.next() {
-            Option::Some(x) => {
-                unsafe {
-                    *((0xb8000 + x * 2) as *mut u16) = (background as u16) << 12;
-                }
-            },
-            Option::None =>{break}
-        }
-    }
-}
+extern crate core;
 
 extern {
-    fn serial_out();
+    fn serial_out(charactor: u8);
+}
+
+fn puts(s: &str) {
+    for c in s.bytes() {
+        unsafe { serial_out(c) };
+    }
 }
 
 #[no_mangle]
 #[no_stack_check]
 pub fn main() {
-    unsafe { serial_out() };
-    clear_screen(Color::LightRed);
+    let string = "Hello My Rust OS!!!";
+    puts(string);
     loop{}
 }
+
--- serial.c
+++ serial.c
@@ -1,6 +1,6 @@
 
-void serial_out()
+void serial_out(unsigned char c)
 {
-	asm volatile("outb %b0, %w1" : : "a"('A'), "Nd"(0x3f8));
+	asm volatile("outb %b0, %w1" : : "a"(c), "Nd"(0x3f8));
 }
 
